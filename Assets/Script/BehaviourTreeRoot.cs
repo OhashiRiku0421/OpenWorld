d934ff9144bc43a29a95e09f2bc25b1f922e60a2
RK <riku.kuu0421@gmail.com>
2023-05-11 23:57:19 +0900

    [add]BehaviourTreeRootクラスを追加

--- Assets/Script/BehaviourTreeRoot.cs
+++ Assets/Script/BehaviourTreeRoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviourTree : MonoBehaviour
+public class BehaviourTreeRoot : MonoBehaviour
 {
 
     void Start()
